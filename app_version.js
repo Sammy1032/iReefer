014dd8c82b7954574f21fcc24550d0e0bddc4ff8
Sammy1032 <sammy1032@livemail.tw>
2018-09-25 23:09:16 +0800

    Update app_version.js

--- app_version.js
+++ app_version.js
@@ -1,8 +1,8 @@
 {
-	"versionCode":1,
-	"versionName":"1.0.0",
-	"fileSize":"35.93M",
-	"apkUrl":"http://www.lofter.com/rsc/android/lofter.apk",
+	"versionCode":2,
+	"versionName":"1.0.1",
+	"fileSize":"7.36M",
+	"apkUrl":"https://github.com/Sammy1032/iReefer/blob/master/app-debug.apk",
 	"required":true,
 	"releaseDate":"2018-09-25 12:45:20",
 	"releaseNotes":["新版本特性新版本特性新版本特性；", "描述版本信息，方便用户选择是否立即下载更新；", "性能优化和BUG修复。"]
