dac237b3a59320c7e029e2cacbf41efb82b7fcff
Sammy1032 <sammy1032@livemail.tw>
2018-09-28 02:15:26 +0800

    Update app_version.js

--- app_version.js
+++ app_version.js
@@ -1,9 +1,9 @@
 {
-	"versionCode":2,
-	"versionName":"1.0.1",
+	"versionCode":3,
+	"versionName":"1.0.2",
 	"fileSize":"7.36M",
 	"apkUrl":"https://raw.githubusercontent.com/Sammy1032/iReefer/master/app-debug.apk",
 	"required":true,
-	"releaseDate":"2018-09-25 12:45:20",
+	"releaseDate":"2018-09-28 12:45:20",
 	"releaseNotes":["新版本特性新版本特性新版本特性；", "描述版本信息，方便用户选择是否立即下载更新；", "性能优化和BUG修复。"]
 }
