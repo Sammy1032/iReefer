49e81a17696526349639da81e59aaebc813deb20
Sammy1032 <sammy1032@livemail.tw>
2018-10-07 21:32:12 +0800

    Update app_version.js

--- app_version.js
+++ app_version.js
@@ -1,9 +1,9 @@
 {
-	"versionCode":2,
-	"versionName":"1.0.1",
+	"versionCode":3,
+	"versionName":"1.0.2",
 	"fileSize":"7.36M",
 	"apkUrl":"https://raw.githubusercontent.com/Sammy1032/iReefer/master/app-debug.apk",
 	"required":true,
-	"releaseDate":"2018-09-30 17:11:20",
-	"releaseNotes":["修正手動GPS時間", "新增LT"]
+	"releaseDate":"2018-10-07 21:31:20",
+	"releaseNotes":["Bug fixes"]
 }
