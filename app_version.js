7e83d0b2533c7251cbc11b50d484d97d3d9150d1
Sammy1032 <sammy1032@livemail.tw>
2019-03-24 00:45:24 +0800

    Update app_version.js

--- app_version.js
+++ app_version.js
@@ -1,9 +1,9 @@
 {
-	"versionCode":8,
-	"versionName":"1.0.7",
+	"versionCode":9,
+	"versionName":"1.0.8",
 	"fileSize":"7.37M",
 	"apkUrl":"https://raw.githubusercontent.com/Sammy1032/iReefer/master/app-debug.apk",
 	"required":true,
-	"releaseDate":"2019-03-04 20:55:20",
+	"releaseDate":"2019-03-24 00:45:20",
 	"releaseNotes":["Adjust txt Format"]
 }
