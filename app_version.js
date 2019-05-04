64a267b98dd6b422da760db23420eb21e65d4583
Sammy1032 <sammy1032@livemail.tw>
2019-05-04 11:43:31 +0800

    Update app_version.js

--- app_version.js
+++ app_version.js
@@ -1,9 +1,9 @@
 {
-	"versionCode":9,
-	"versionName":"1.0.8",
-	"fileSize":"7.37M",
+	"versionCode":10,
+	"versionName":"1.0.9",
+	"fileSize":"7.70M",
 	"apkUrl":"https://raw.githubusercontent.com/Sammy1032/iReefer/master/app-debug.apk",
 	"required":true,
-	"releaseDate":"2019-03-24 00:45:20",
-	"releaseNotes":["Adjust txt Format"]
+	"releaseDate":"2019-05-04 11:42:20",
+	"releaseNotes":["Enhance GPS, bug fixes"]
 }
